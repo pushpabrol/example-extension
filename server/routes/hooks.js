380c4f612e0bcfebd8bf13c8e4f8920265438903
Pushp Abrol <pushpabrol@macbook-pro-4.lan>
2020-05-22 13:42:31 -0400

    add rules etc

--- server/routes/hooks.js
+++ server/routes/hooks.js
@@ -49,18 +49,7 @@ export default () => {
   // there are three types of hooks: `on-install`, `on-update` and `on-uninstall`
   hooks.delete('/on-uninstall', (req, res) => {
     logger.debug('Uninstall running...');
-    req.auth0.clients.delete({ client_id: config('AUTH0_CLIENT_ID') })
-      .then(() => {
-        logger.debug(`Deleted client: ${config('AUTH0_CLIENT_ID')}`);
-        res.sendStatus(204);
-      })
-      .catch((err) => {
-        logger.debug(`Error deleting client: ${config('AUTH0_CLIENT_ID')}`);
-        logger.error(err);
 
-        // even if deleting fails, we need to be able to uninstall the extension.
-        res.sendStatus(204);
-      });
   });
   return hooks;
 };
