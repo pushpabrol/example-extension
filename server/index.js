615b907aa193307085c4785789bf813b64131e2f
Pushp Abrol <pushpabrol@macbook-pro-4.lan>
2020-05-22 16:31:30 -0400

    add rules etc

--- server/index.js
+++ server/index.js
@@ -3,10 +3,10 @@ import Express from 'express';
 import * as tools from 'auth0-extension-tools';
 import { middlewares, routes } from 'auth0-extension-express-tools';
 
-import api from './routes/api';
+//import api from './routes/api';
 import hooks from './routes/hooks';
 import meta from './routes/meta';
-import htmlRoute from './routes/html';
+//import htmlRoute from './routes/html';
 import config from './lib/config';
 import logger from './lib/logger';
 
@@ -26,13 +26,13 @@ export default function (cfg, storageProvider) {
   logger.debug("config....");
     logger.debug(config('AUTH0_DOMAIN'));
 
-  app.use('/api', api(storage));
-  app.use('/app', Express.static(path.join(__dirname, '../dist')));
+  //app.use('/api', api(storage));
+  //app.use('/app', Express.static(path.join(__dirname, '../dist')));
   app.use('/meta', meta());
   app.use('/.extensions', hooks());
 
   // Fallback to rendering HTML.
-  app.get('*', htmlRoute());
+ // app.get('*', htmlRoute());
 
   // Generic error handler.
   app.use(middlewares.errorHandler(logger.error.bind(logger)));
