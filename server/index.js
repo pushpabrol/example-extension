da708c99ed74e1e4933fa4f07f1814b8699c011c
Pushp Abrol <pushpabrol@macbook-pro-4.lan>
2020-05-22 17:03:11 -0400

    add rules etc

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import path from 'path';
 import Express from 'express';
+import * as tools from 'auth0-extension-tools';
 import { middlewares, routes } from 'auth0-extension-express-tools';
+import api from './routes/api';
 import hooks from './routes/hooks';
 import meta from './routes/meta';
 import config from './lib/config';
@@ -9,11 +11,21 @@ import logger from './lib/logger';
 export default function (cfg, storageProvider) {
   // config initialization
   config.setProvider(cfg);
+
+
+  // storage initialization
+  const storage = storageProvider
+    // use webtask storage if possible
+    ? new tools.WebtaskStorageContext(storageProvider)
+    // use local file storage if webtask storage unavailable
+    : new tools.FileStorageContext(path.join(__dirname, './data.json'));
+
   const app = new Express();
 
   logger.debug("config....");
   logger.debug(config('AUTH0_DOMAIN'));
 
+  app.use('/api', api(storage));
   app.use('/meta', meta());
   app.use('/.extensions', hooks());
 
